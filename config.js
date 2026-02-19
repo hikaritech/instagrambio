fa6ce3e834a7bcec69d2537821d165ee17561086
seeguns12 <faiznyo12@gmail.com>
2026-02-19 16:33:32 +0700

    new

---
+++ config.js
@@ -0,0 +1,6 @@
+// JSONBin.io Configuration
+const JSONBIN_CONFIG = {
+  apiKey: '$2a$10$NLdFXBAirvJf9wVXl.Sxne7TScVxGba6KeCk.efym1HTyYn4GzNjW',
+  binId: '', // Will be created on first save
+  apiUrl: 'https://api.jsonbin.io/v3'
+};
